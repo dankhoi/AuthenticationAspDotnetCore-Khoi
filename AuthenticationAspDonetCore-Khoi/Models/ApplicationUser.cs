b00bd13f4783acc0c42b721ed30e7495fd3cd76d
Trần Đoàn Đăng Khôi <khoitddgcs190844@fpt.edu.vn>
2022-01-10 16:14:47 +0700

    Add ApplicationUser

---
+++ AuthenticationAspDonetCore-Khoi/Models/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace AuthenticationAspDonetCore_Khoi.Models
+{
+    public class ApplicationUser: IdentityUser
+    {
+        public string FullName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string CredentialId { get; set; }
+        public string HealthCareId { get; set; }
+    }
+}
